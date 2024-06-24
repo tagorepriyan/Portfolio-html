158bf488816e39af6f6dad003c4e4949f144d8c1
Tagorepriyan <137099662+tagorepriyan@users.noreply.github.com>
2024-06-24 15:33:53 +0530

    Update script.js
    JS updated with error msg

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 function copyPhoneNumber() {
     var phoneNumber = "+91 6383759915";
     navigator.clipboard.writeText(phoneNumber).then(function() {
-      alert("Phone number copied!");
+        alert("Phone number copied!");
+    }).catch(function(error) {
+        alert("Failed to copy phone number: " + error);
     });
-  }
+}
 
-  function copyEmail() {
+function copyEmail() {
     var email = "tagorepryansaravanavel@gmail.com";
     navigator.clipboard.writeText(email).then(function() {
-      alert("Email copied!");
+        alert("Email copied!");
+    }).catch(function(error) {
+        alert("Failed to copy email: " + error);
     });
-  }
+}
 
-  function goBack() {
+function goBack() {
     window.location.href = "../index.html";
-  }
+}
